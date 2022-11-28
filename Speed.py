c5f8b8dd3b01d141f7b1f0fe3c143f28a688eac1
ramtarunk <119341534+ramtarunk@users.noreply.github.com>
2022-11-28 10:19:33 -0600

    Add files via upload

---
+++ Speed.py
@@ -0,0 +1,3 @@
+n_miles=int(input("Enter the distance : "))
+miles_speed=int(input("enter the speed per hour : "))
+print("with given speed it can reach the destination in", int(n_miles/miles_speed),"hours")
