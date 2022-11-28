75ba133e67cc36be8cf36470dd035ff696258f27
ramtarunk <119341534+ramtarunk@users.noreply.github.com>
2022-11-28 11:04:05 -0600

    Update Speed.py

--- Speed.py
+++ Speed.py
@@ -1,3 +1,5 @@
 n_miles=int(input("Enter the distance : "))
 miles_speed=int(input("enter the speed per hour : "))
 print("with given speed it can reach the destination in", int(n_miles/miles_speed),"hours")
+
+print("HI ramtarun")
